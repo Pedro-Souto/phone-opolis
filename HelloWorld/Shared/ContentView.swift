38ce00aa4c2ff8531d5e89f0c0a1d9d408417f37
Pedro Souto <pedro.souto@tecnico.ulisboa.pt>
2021-09-14 00:16:21 +0100

    lesson 01 - Coursera

---
+++ HelloWorld/Shared/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Pedro Souto on 14/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world! It's Pedro Calheiros Souto here!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ HelloWorld/Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by Pedro Souto on 14/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
